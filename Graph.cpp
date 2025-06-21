18d5a4b0cbfc4bf938a79980146e6916dfb15d47
minzoo_0_0 <32029953+mz7048@users.noreply.github.com>
2025-06-21 07:07:04 +0000

    adding Graph.cpp

---
+++ Graph.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+
+class graph{
+    int numOfVectex;
+    int numOfEdge;
+    vector<int> graph;
+
+    void addGraph(){
+        
+    }
+
+    graph(int v,int e){
+        graph = new vector<int>();
+        numOfVectex = v;
+        numOfEdge = e;
+    }
+}
+
+int main(){
+    
+}
